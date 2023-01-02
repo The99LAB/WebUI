2fcd16367017199a2b0bb2cc92addcdae9d7f4f5
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-01-02 19:54:39 +0100

    MainLayout: Remove img and proper title

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -3,12 +3,8 @@
     <q-header class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+          Server3
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
