3f0f3f5ebe4f9f6489705debebb9d662dc577367
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-01-27 20:49:47 +0100

    format routes.js

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -6,11 +6,23 @@ const routes = [
       { path: "", redirect: "/dashboard" },
       { path: "dashboard", component: () => import("pages/DashboardPage.vue") },
       { path: "vm-manager", component: () => import("pages/VmPage.vue") },
-      { path: "storage-pools", component: () => import("src/pages/StoragePoolPage.vue") },
-      { path: "tools", component: () => import("pages/ToolsPage.vue")},
-      { path: "tools/system-info", component: () => import("pages/SystemInfoPage.vue") },
-      { path: "tools/system-devices", component: () => import("pages/SystemDevicesPage.vue") },
-      { path: "tools/system-logs", component: () => import("pages/SystemLogsPage.vue") },
+      {
+        path: "storage-pools",
+        component: () => import("src/pages/StoragePoolPage.vue"),
+      },
+      { path: "tools", component: () => import("pages/ToolsPage.vue") },
+      {
+        path: "tools/system-info",
+        component: () => import("pages/SystemInfoPage.vue"),
+      },
+      {
+        path: "tools/system-devices",
+        component: () => import("pages/SystemDevicesPage.vue"),
+      },
+      {
+        path: "tools/system-logs",
+        component: () => import("pages/SystemLogsPage.vue"),
+      },
       { path: "tools/about", component: () => import("pages/AboutPage.vue") },
     ],
   },
