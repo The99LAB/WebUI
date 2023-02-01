3a0ec1b388f03704ccd0436aefaf3942907bc955
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-02-01 16:07:27 +0100

    SystemDevicesPage: Add USB devices table and add loading

--- frontend/src/pages/SystemDevicesPage.vue
+++ frontend/src/pages/SystemDevicesPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
-    <q-table :rows="pcieTableRows" :columns="pcieTableColumns" title="PCIe Devices" row-key="uuid" separator="none"
-      :pagination="pcieTablepaginationOptions">
+    <q-table :loading="pciTableLoading" :rows="pcieTableRows" :columns="pcieTableColumns" title="PCIe Devices" row-key="uuid" separator="none"
+      :pagination="pcieTablePaginationOptions">
       <template #body="props">
         <q-tr :props="props">
           <q-td key="iommuGroup" :props="props">
@@ -35,8 +35,9 @@
       </template>
     </q-table>
     <q-separator spaced="lg" inset />
-    <q-table title="SCSI Devices" :rows="scsiTableRows" :columns="scsiTablecolumns" row-key="path"
-      :pagination="scsiTablepaginationOptions" />
+    <q-table :loading="scsiTableLoading" title="SCSI Devices" :rows="scsiTableRows" :columns="scsiTableColumns" row-key="path" separator="none" :pagination="scsiTablePaginationOptions"/>
+    <q-separator spaced="lg" inset />
+    <q-table :loading="usbTableLoading" title="USB Devices" :rows="usbTableRows" :columns="usbTableColumns" row-key="name" separator="none" :pagination="usbTablePaginationOptions"/>
   </q-page>
 </template>
 
@@ -53,26 +54,47 @@ const pcieTableColumns = [
 
 const scsiTableRows = []
 
-const scsiTablecolumns = [
+const scsiTableColumns = [
   { label: 'Name', field: 'model', name: 'model', align: 'left' },
   { label: 'Type', field: 'type', name: 'type', align: 'left' },
   { label: 'Path', field: 'path', name: 'path', align: 'left' },
   { label: 'Size', field: 'capacity', name: 'capacity', align: 'left' }
 ]
 
+const usbTableRows = []
+
+const usbTableColumns = [
+  { label: 'Name', field: 'name', name: 'name', align: 'left' },
+  { label: 'Path', field: 'path', name: 'path', align: 'left' },
+  { label: 'Id', field: 'id', name: 'id', align: 'left' }
+]
+
 export default {
   data() {
+
+    const pciTableLoading = ref(true)
+    const scsiTableLoading = ref(true)
+    const usbTableLoading = ref(true)
+
     return {
       pcieTableRows,
       pcieTableColumns,
-      pcieTablepaginationOptions: {
+      pcieTablePaginationOptions: {
         sortBy: 'iommuGroup',
       },
       scsiTableRows,
-      scsiTablecolumns,
+      scsiTableColumns,
       scsiTablePaginationOptions: {
         sortBy: 'path',
-      }
+      },
+      usbTableRows,
+      usbTableColumns,
+      usbTablePaginationOptions: {
+        sortBy: 'path',
+      },
+      pciTableLoading,
+      scsiTableLoading,
+      usbTableLoading
     }
   },
   components: {
@@ -81,15 +103,23 @@ export default {
     updatePcieDevices() {
       this.$api.get("/host/system-devices/pcie").then((response) => {
         this.pcieTableRows = response.data
+        this.pciTableLoading = false
       }).catch((error) => {
         this.$refs.errorDialog.show(error.response.data)
       })
     },
     updateScsiDevices() {
       this.$api.get("/host/system-devices/scsi").then((response) => {
-        console.log("abcd")
-        console.log(response.data)
         this.scsiTableRows = response.data
+        this.scsiTableLoading = false
+      }).catch((error) => {
+        this.$refs.errorDialog.show(error.response.data)
+      })
+    },
+    updateUsbDevices() {
+      this.$api.get("/host/system-devices/usb").then((response) => {
+        this.usbTableRows = response.data
+        this.usbTableLoading = false
       }).catch((error) => {
         this.$refs.errorDialog.show(error.response.data)
       })
@@ -99,6 +129,7 @@ export default {
   mounted() {
     this.updatePcieDevices()
     this.updateScsiDevices()
+    this.updateUsbDevices()
   },
   beforeUnmount() {
   }
