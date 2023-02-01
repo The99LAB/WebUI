1d7e582ef1a9eb9562a78f03c4bc9444c081e8ab
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-02-01 16:06:56 +0100

    SystemInfoPage: get data from backend and add loading

--- frontend/src/pages/SystemInfoPage.vue
+++ frontend/src/pages/SystemInfoPage.vue
@@ -1,45 +1,56 @@
 <template>
   <q-page padding>
     <q-card>
+      <q-inner-loading :showing="loadingVisible">
+        <q-spinner-gears size="50px" color="primary" />
+      </q-inner-loading>
       <q-card-section>
-        <div class="text-h6 text-center">System Information</div>
+        <div class="text-h6 text-center" v-show="showData">System Information</div>
       </q-card-section>
       <q-separator dark inset />
       <q-card-section>
-        <div class="row justify-center text-body2">
+        <div class="row justify-center text-body2" v-show="showData">
           <div class="col text-right q-mr-sm text-weight-bold">
             <p>Motherboard</p>
             <p>Processor</p>
             <p>Memory</p>
             <p>Operating System</p>
+            <p>Linux Version</p>
             <p>Host Name</p>
           </div>
-          <div class="col text-weight-regular">
+          <div class="col text-weight-regular" v-show="showData">
             <p>{{ motherboard }}</p>
             <p>{{ processor }}</p>
             <p>{{ memory }}</p>
             <p>{{ os }}</p>
+            <p>{{ linuxVersion }}</p>
             <p>{{ hostname }}<q-btn round icon="edit" flat text-color="primary" size="sm" padding="none"
                 @click="editHostName()"></q-btn></p>
           </div>
         </div>
       </q-card-section>
+      
     </q-card>
-    <editHostName ref="editHostNameDialog" />
+    <editHostName ref="editHostNameDialog" @hostname-edit-finished="getSystemInfo()"/>
   </q-page>
 </template>
 
 <script>
 import editHostName from '../components/EditHostName.vue'
+import { ref } from 'vue'
 export default {
   data() {
+    const loadingVisible = ref(true)
+    const showData = ref(false)
     return {
-      motherboard: "MSI A320 Pro Max",
-      processor: "Ryzen 5 3600X",
-      memory: "4GB",
-      os: "Fedora 36 (server)",
-      hostname: "Server3",
-
+      motherboard: "",
+      processor: "",
+      memory: "",
+      os: "",
+      hostname: "",
+      linuxVersion: "",
+      loadingVisible,
+      showData,
     }
   },
   components: {
@@ -48,11 +59,28 @@ export default {
   methods: {
     getSystemInfo() {
       console.log('Getting system info');
+      this.$api.get("/host/system-info/all").then((response) => {
+        console.log("System info: ", response.data)
+        this.motherboard = response.data.motherboard
+        this.processor = response.data.processor
+        this.memory = response.data.memory
+        this.os = response.data.os
+        this.hostname = response.data.hostname
+        this.linuxVersion = response.data.linuxVersion
+        this.loadingVisible = false
+        this.showData = true
+
+      }).catch((error) => {
+        console.log("Error getting system info: ", error.response.data)
+      })
     },
     editHostName() {
       console.log('Editing host name');
-      this.$refs.editHostNameDialog.show()
+      this.$refs.editHostNameDialog.show(name=this.hostname)
     }
   },
+  mounted() {
+    this.getSystemInfo()
+  }
 }
 </script>
