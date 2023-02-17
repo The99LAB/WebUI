a5e65c04eef491fd1d3b956b370d031d79e28fef
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-02-17 17:36:23 +0100

    Fix some browser warnings

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -49,6 +49,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      polyfillModulePreload: true,
       target: {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node16",
