f94cf24dece5a8b817f31093dcea82b8f2f5dbe6
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-02-24 16:43:28 +0100

    LoginPage: redesign

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,42 +1,79 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-page-container>
-      <q-page padding>
-        <h5>Login</h5>
-        <q-input v-model="username" label="Username" />
-        <q-input v-model="password" label="Password" type="password" />
-        <q-separator spaced inset dark />
-        <q-btn color="primary" icon="check" label="Login" @click="login" />
-        <!-- error -->
-        <q-separator spaced inset dark />
-        <p class="text-body2 text-weight-bold text-negative">{{ authError }}</p>
-      </q-page>
-    </q-page-container>
-  </q-layout>
-</template>
+    <q-layout view="lHh Lpr lFf">
+        <q-page-container>
+            <q-page padding class=" window-height window-width row justify-center items-center">
+                <q-card square class="shadow-24" style="width:400px;height:540px;">
+                    <q-card-section class="bg-primary">
+                        <h5 class="text-white text-center q-my-md">Login</h5>
+                    </q-card-section>
+                    <q-card-section>
+                        <div class="q-px-sm q-pt-xl">
+                            <q-input 
+                                square 
+                                clearable 
+                                v-model="username" 
+                                label="Username"
+                                @update:model-value="authError = ''">
+                                <template v-slot:prepend>
+                                    <q-icon name="mdi-account" />
+                                </template>
+                            </q-input>
+                            <q-input  
+                                square 
+                                clearable 
+                                v-model="password"
+                                type="password"  
+                                label="Password"
+                                @update:model-value="authError = ''">
+                                <template v-slot:prepend>
+                                    <q-icon name="mdi-lock" />
+                                </template>
+                            </q-input>
+                            <q-separator spaced inset dark />
+                            <p class="text-body2 text-weight-bold text-center text-negative">{{ authError }}</p>
+                        </div>
+                    </q-card-section>
 
+                    <q-card-actions class="q-px-lg">
+                        <q-btn 
+                        size="lg" 
+                        color="primary"
+                        class="full-width"                          
+                        label="Login" 
+                        @click="login"
+                        :loading="loginLoading"/>
+                    </q-card-actions>
+                </q-card>
+            </q-page>
+        </q-page-container>
+    </q-layout>
+</template>
 <script>
+
 export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-      authError: "",
-    };
-  },
-  methods: {
-    login() {
-      this.$api
-        .post("login", { username: this.username, password: this.password })
-        .then((response) => {
-          this.authError = "";
-          localStorage.setItem("jwt-token", response.data.access_token);
-          this.$router.push({ path: "/dashboard" });
-        })
-        .catch((error) => {
-          this.authError = error.response.data;
-        });
+    data() {
+        return {
+            username: '',
+            password: '',
+            authError: '',
+            loginLoading: false
+        }
     },
-  },
-};
+    methods: {
+        login() {
+            this.loginLoading = true
+            this.$api.post('login', { "username": this.username, "password": this.password })
+                .then(response => {
+                    this.authError = ''
+                    localStorage.setItem("jwt-token", response.data.access_token)
+                    this.$router.push({ path: '/dashboard' })
+                    this.loginLoading = false
+                })
+                .catch(error => {
+                    this.authError = error.response.data
+                    this.loginLoading = false
+                })
+        },
+    }
+}
 </script>
