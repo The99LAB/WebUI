0491c6c5e451d9b2517a008d5ce09d0180adfa41
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-04-01 11:56:13 +0200

    Update AboutPage.vue

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -5,7 +5,7 @@
         <div class="text-h6">About</div>
       </q-card-section>
       <q-separator dark inset />
-      <q-card-section> Version: 1.0.0 </q-card-section>
+      <q-card-section> Version: 1.0 </q-card-section>
     </q-card>
   </q-page>
 </template>
