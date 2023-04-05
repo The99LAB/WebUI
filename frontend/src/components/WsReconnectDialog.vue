08012eee32ee4814ddb45a2fff088b5d16692435
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-04-05 10:01:39 +0200

    WsReconnectDialog: fix design

--- frontend/src/components/WsReconnectDialog.vue
+++ frontend/src/components/WsReconnectDialog.vue
@@ -2,17 +2,26 @@
     <q-dialog v-model="alert">
       <q-card>
         <q-card-section class="row items-center q-pb-none">
-          <div class="text-h6">WebSocket closed. Reconnect?</div>
+          <div class="text-h6">WebSocket closed</div>
           <q-space />
           <q-btn icon="close" flat round dense v-close-popup />
         </q-card-section>
         <q-separator color="transparent" spaced="lg" inset />
         <q-card-section class="q-pt-none q-px-xl">
-            <q-btn label="Reconnect" @click="reconnect()" />
+          <div class="row justify-center">
+            <p>Do you want to reconnect?</p>
+          </div>
+          <div class="row justify-center">
+            <q-btn
+              flat
+              class="q-ma-sm"
+              color="primary"
+              icon="mdi-refresh"
+              label="Reconnect"
+              @click="reconnect"
+            />
+          </div>
         </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Finish" @click="addDisk()" />
-        </q-card-actions>
       </q-card>
     </q-dialog>
 </template>
