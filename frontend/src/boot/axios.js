aeea8b511be4e851e0f407073a8cb84b5aa13af8
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-07-11 21:46:43 +0200

    Frontend: Axios: Fix undefined response

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -33,7 +33,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response.status === 401) {
+    if (error.response != undefined && error.response.status === 401) {
       localStorage.removeItem("jwt-token");
 
       // if current page is not login page, reload page, which will redirect to login page
