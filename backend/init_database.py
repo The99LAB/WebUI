50a0a17a1866d5361e7013dcfa53f2508c2fb2e5
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-11-08 20:59:44 +0100

    Docker: Create template repo update setting

--- backend/init_database.py
+++ backend/init_database.py
@@ -18,6 +18,7 @@ novnc_protocool_setting = Setting("novnc_protocool", novnc_protocool, "Protocool
 novnc_path_setting = Setting("novnc_path", novnc_path, "Path to novnc server")
 libvirt_domain_logs_path_setting = Setting("libvirt_domain_logs", libvirt_domain_logs_path, "Path to libvirt domain logs", verifyPath=True)
 login_token_expire_setting = Setting("login_token_expire", "3600", "Login token expire time in seconds", regex="^[0-9]+$", regex_description="Only numbers allowed")
+docker_template_repository_update = Setting("docker_template_repository_update", "0 0 * * *", "Cron expression for docker template repository update", regex="^(\*|[0-9]+(\/[0-9]+)?)(\s(\*|[0-9]+(\/[0-9]+)?)){4}$", regex_description="Only cron expression allowed")
 settings_manager.create_setting(qemu_path_setting)
 settings_manager.create_setting(novnc_ip_setting)
 settings_manager.create_setting(novnc_port_setting)
@@ -25,6 +26,7 @@ settings_manager.create_setting(novnc_protocool_setting)
 settings_manager.create_setting(novnc_path_setting)
 settings_manager.create_setting(libvirt_domain_logs_path_setting)
 settings_manager.create_setting(login_token_expire_setting)
+settings_manager.create_setting(docker_template_repository_update)
 
 # Create ovmf paths
 ovmf_path = "/usr/share/OVMF/OVMF_CODE.fd"
